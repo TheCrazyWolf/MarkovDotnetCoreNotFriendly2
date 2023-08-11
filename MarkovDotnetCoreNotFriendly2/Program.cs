bee26340d8d9c85971fff100f0a7ebd2a83bd844
Кулагин Алексей <tcrazywolf63@gmail.com>
2023-08-11 13:02:09 +0400

    Добавьте файлы проекта.

---
+++ MarkovDotnetCoreNotFriendly2/Program.cs
@@ -0,0 +1,49 @@
+﻿using MarkovDotnetCoreNotFriendly2.Saver;
+using MarkovSharp.TokenisationStrategies;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarkovDotnetCoreNotFriendly2
+{
+    internal class Program
+    {
+        private static Manager _manager = new Manager();
+
+        static void Main(string[] args)
+        {
+            //_manager.OnLoadDataFirst();
+            //return;
+
+            // Create a new model
+            var model = new StringMarkov(1);
+
+            // Train the model
+            model.Learn(_manager.OnLoadData());
+
+            int i = 0;
+            // Output:
+            // Frankly, my dear, I don't give a box of their possessions.
+            while (true)
+            {
+                i++;
+
+                //var sentense_new = model.Walk(3).ToArray();
+                //string sentense_new_string = sentense_new[0] + sentense_new[1] + sentense_new[2];
+                //Console.WriteLine($"{i}) {sentense_new_string}");
+
+                var sentense_new = model.Walk().ToArray();
+                string sentense_new_string = sentense_new[0];
+                Console.WriteLine($"{i}) {sentense_new_string}");
+
+                //model.Learn(sentense_new_string);
+
+                //_manager.PutData(sentense_new_string);
+            }
+
+        }
+    }
+}
---
+++ MarkovDotnetCoreNotFriendly2/Saver/Manager.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace MarkovDotnetCoreNotFriendly2.Saver
+{
+    internal class Manager
+    {
+        private List<string> sentances = new List<string>();
+        private string URL = "bigdata.json";
+
+        public List<string> OnLoadDataFirst()
+        {
+            var file = File.ReadAllText(URL);
+            sentances = file.Split('\n').ToList();
+
+            var json = JsonConvert.SerializeObject(sentances);
+            File.WriteAllText($"{URL}.json", json);
+
+            return sentances;
+        }
+
+        public List<string> OnLoadData()
+        {
+            var json = File.ReadAllText(URL);
+            sentances = JsonConvert.DeserializeObject<List<string>>(json);
+            return sentances;
+        }
+
+        public void PutData(string data)
+        {
+            sentances.Add(data);
+            SaveData();
+        }
+
+        public void SaveData()
+        {
+            var json = JsonConvert.SerializeObject(sentances);
+            File.WriteAllText(URL, json);
+        }
+    }
+}
